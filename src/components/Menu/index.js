e5893fcd80cbe25ff855e0ce6e0c7e97555faa76
brunoalbrito <bruno.brito@clickbus.com>
2020-07-31 20:33:46 -0300

    Ajusting more code style

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
-//import ButtonLink from './components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 import './Menu.css';
 import Button from '../Button';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-function Menu(){
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt="AluraFlix logo"/>
-            </Link>
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-            <Button as={Link} className="ButtonLink" to="/cadastro/video">
-                Novo Vídeo
-            </Button>
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
